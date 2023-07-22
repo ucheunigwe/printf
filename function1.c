6524d505cebe376de3580fb94ffa881674616114
Mariam Ladejobi <132400342+fluffymariam@users.noreply.github.com>
2023-07-22 10:04:50 +0100

    Update function1.c

--- function1.c
+++ function1.c
@@ -4,8 +4,8 @@
 /**
  * print_unsigned - Prints an unsigned number
  * @types: List a of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
+ * @buffer: Buffer array to handle print function
+ * @flags:  Calculates the active flags
  * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
